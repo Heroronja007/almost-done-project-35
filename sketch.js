96478489447bcb9917e31807c87eb56301c1818d
Heroronja007 <65021896+Heroronja007@users.noreply.github.com>
2020-08-11 15:44:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+//Create variables here
+var dog, happyDog, milk,  database, foodS, foodStock;
+var dogImg, happyDogImg;
+var position
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+//Create variables here
+
+function preload()
+
+{
+  dogImg = loadImage("Dog.png")
+  happyDogImg = loadImage("happydog.png")
+	//load images here
+}
+
+function setup() {
+	createCanvas(1000, 500);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogImg)
+  dog.scale=0.2
+ 
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+  feed = createButton("FEED DRAGO")
+  feed.position(500,15)
+  feed.mousePressed(FeedDog)
+  add = createButton("ADD FOOD")
+  add.position(400,15)
+  add.mousePressed(AddFood)
+
+} 
+
+function draw(){
+ background(46,139,87);
+
+ foodobject.display()
+ 
+ drawSprites();
+  
+ fill(255,255,254);
+ textSize(15);
+
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(nazo){
+  if(nazo>0){
+    nazo=nazo-1
+  }
+  else{
+    nazo=0
+  }
+  database.ref('/').set({
+    'Food': nazo
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function FeedDog(){
+
+dog.addImage(happyDogImg)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
+}
